96c14047423c291022412c1e02682bcf3f059e48
vitozs <vitornuneschagas2016@gmail.com>
2023-09-30 18:07:07 -0300

    Feat(Repository): Aplicada as boas praticas com a notacao @Repository

--- src/main/java/com/example/springboot/repository/ProdutoRepository.java
+++ src/main/java/com/example/springboot/repository/ProdutoRepository.java
@@ -3,8 +3,8 @@ package com.example.springboot.repository;
 import com.example.springboot.model.Produto;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
 
-
+@Repository
 public interface ProdutoRepository extends CrudRepository<Produto, Integer> {
-
 }
